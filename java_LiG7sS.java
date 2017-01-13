2c5447c1ceb7884b8de577a342cc139f40503a51
Natalia Barsova <natfast@gmail.com>
2017-01-13 17:57:46 +0100

    Problem: what is the first Fibonacci number with X digits

--- java_LiG7sS.java
+++ java_LiG7sS.java
@@ -1,21 +1,14 @@
-//*******************************************************************
-// NOTE: please read the 'More Info' tab to the right for shortcuts.
-//*******************************************************************
-
-import java.lang.Math; // headers MUST be above the first class
-
-// one class needs to have a main() method
+/
 public class HelloWorld
 {
-  // arguments are passed using the text field below this editor
   public static void main(String[] args)
   {
     
-    int digits = 1;
+    int x = 1;
     
     int border = 1;
     
-    for (int i=0; i<digits; i++)
+    for (int i=0; i<x; i++)
     {
       border = border*10;
     }
@@ -30,7 +23,7 @@ public class HelloWorld
       currentFib+=prevFib;
     }
     
-    System.out.println ("Smallest fibbonacci number with "+digits+1+" digits is" + currentFib);
+    System.out.println ("Smallest fibbonacci number with "+x+1+" digits is" + currentFib);
     
   }
   
