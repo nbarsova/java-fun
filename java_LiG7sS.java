2b1272e30b53383992258ed5a9792d3912a7d6fc
Natalia Barsova <natfast@gmail.com>
2017-01-13 22:56:36 +0100

    Update java_LiG7sS.java

--- java_LiG7sS.java
+++ java_LiG7sS.java
@@ -26,7 +26,27 @@ public class HelloWorld
     System.out.println ("Smallest fibbonacci number with "+x+1+" digits is" + currentFib);
     
   }
+/*  
+  private static long fibonacci(int n) {
+        if(n < 1) {
+            throw new IllegalArgumentException("Minimal length of any number is 1");
+        }
+        
+        if(n == 1) {
+            return 0;
+        }
+        long compareTo = (int) Math.pow(10, n - 1);
+        long f1 = 1;
+        long f2 = 1;
+        long f3 = 2;
+        while(f3 < compareTo) {
+            f1 = f2;
+            f2 = f3;
+            f3 = f1 + f2;
+        }
+        return f3;
+    }
   
- 
+ */
 }
 
